7c75993c9a0f0b61eb38cb631adaee7997878973
xabn12 <fabriciomanayay@gmail.com>
2026-03-06 00:21:29 -0500

    3th upload

--- bank.py
+++ bank.py
@@ -13,7 +13,26 @@ def smooth(texto, velocidad=0.05):
     
 name="Fabricio"
 
-smooth("Hola "+ name + " que operacion deseas realizar")
+smooth("Hola " + name + " que operacion deseas realizar")
 
-operacion=input("1.-Retiro  2.-Deposito :    ")
+while True:
+    operacion= int(input("1.- Retiro  2.- Deposito :    "))
+    
+    match operacion:
+        case 1: 
+             print("Seleccionaste: Retiro")
+             retiro=input("Cuanto desea retirar:   ")
+             if retiro < 10:
+                 print("El monto no puede ser menor de 10")
+             else:
+                 print("Retiro exitoso")
+             break
+        case 2:
+             print("Seleccionaste: Depósito")
+             break
+        case _:
+             print("Esa operación no existe")
+             break
+    
+
 
---
+++ prueba2.py
@@ -0,0 +1,5 @@
+contador = 0
+
+while contador < 3:
+    print(f"El contador es: {contador}")
+    contador += 1  # Es vital cambiar la condición para no crear un bucle infinito
